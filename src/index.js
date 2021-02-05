45c37730be14ca78baa57587743ff168a9c27004
Fifer2 <KuryakovDenis@yandex.ru>
2021-02-05 11:03:51 +0700

    feat(app): creating app component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainScreen from "./components/main/main";
+import App from "./components/app/app";
 
 ReactDOM.render(
-    <MainScreen/>,
+    <App/>,
     document.querySelector(`#root`)
 );
