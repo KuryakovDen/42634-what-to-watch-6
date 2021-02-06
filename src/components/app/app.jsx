bd6bc852bb1f9b7c24b020a1bb86efa10b508418
Fifer2 <KuryakovDenis@yandex.ru>
2021-02-06 14:51:19 +0700

    feat(app): implementation of routing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
 import MainScreen from "../main/main";
 import PropTypes from 'prop-types';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import SignIn from "../sign-in/sign-in";
+import MyList from "../my-list/my-list";
+import Movie from "../movie/movie";
+import Review from "../review/review";
+import Player from "../player/player";
+import NotFound from "../not-found/not-found";
 
 const App = (props = {}) => (
-  <MainScreen {...props} />
+  <>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={`/`}>
+          <MainScreen {...props} />
+        </Route>
+        <Route exact path={`/login`}>
+          <SignIn></SignIn>
+        </Route>
+        <Route exact path={`/mylist`}>
+          <MyList></MyList>
+        </Route>
+        <Route exact path={`/films/:id`}>
+          <Movie></Movie>
+        </Route>
+        <Route exact path={`/films/:id/review`}>
+          <Review></Review>
+        </Route>
+        <Route exact path={`/player/:id`}>
+          <Player></Player>
+        </Route>
+        <Route>
+          <NotFound></NotFound>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </>
 );
 
 App.propTypes = {
