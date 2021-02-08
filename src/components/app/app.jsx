0a0f3e2898c26d05f5f926964ed8c0687d358459
Fifer2 <KuryakovDenis@yandex.ru>
2021-02-08 14:11:15 +0700

    refactor(app): refactoring for imports

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import MainScreen from "../main/main";
 import PropTypes from 'prop-types';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
+
+import MainScreen from "../main/main";
 import SignIn from "../sign-in/sign-in";
 import MyList from "../my-list/my-list";
 import Movie from "../movie/movie";
