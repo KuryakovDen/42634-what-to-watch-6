837f41eff9cc583e2db03c5a3f11c4425983c050
Fifer2 <KuryakovDenis@yandex.ru>
2021-02-16 12:57:35 +0700

    feat(app): using history API

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import {useHistory} from "react-router-dom";
 
 import {promoMovieValidation} from "../../validation";
 
-const MovieCard = ({movie = {}, cardHoverHandler = {}}) => (
-  <>
-    <article className="small-movie-card catalog__movies-card" onMouseOver={() => {
-      cardHoverHandler(movie.id);
-    }}>
-      <div className="small-movie-card__image">
-        <img src={movie.preview_image}
-          alt={movie.name} width="280" height="175"/>
-      </div>
-      <h3 className="small-movie-card__title">
-        <Link className="small-movie-card__link" to={`/films/1`}>{movie.name}</Link>
-      </h3>
-    </article>
-  </>
-);
+const MovieCard = ({movie = {}, cardHoverHandler = {}}) => {
+  const history = useHistory();
+  return (
+    <>
+      <article className="small-movie-card catalog__movies-card" onMouseOver={() => {
+        cardHoverHandler(movie.id);
+      }}>
+        <div className="small-movie-card__image">
+          <img src={movie.preview_image}
+            alt={movie.name} width="280" height="175"/>
+        </div>
+        <h3 className="small-movie-card__title">
+          <Link className="small-movie-card__link" to={`/films/${movie.id}`} onClick={(evt) => {
+            evt.preventDefault();
+            history.push(`/films/${movie.id}`);
+          }}>{movie.name}</Link>
+        </h3>
+      </article>
+    </>
+  );
+};
 
 MovieCard.propTypes = {...promoMovieValidation};
 
