ea3220a747e2dc42d4d4e1282486c686cd79a15f
Fifer2 <KuryakovDenis@yandex.ru>
2021-03-11 14:43:08 +0700

    refactor(movie-reviews): refactoring for movie-reviews view in grid

--- src/components/movie-reviews/movie-reviews.jsx
+++ src/components/movie-reviews/movie-reviews.jsx
@@ -4,11 +4,17 @@ import MovieReview from "../movie-review/movie-review";
 import {reviewsType} from "../../validation";
 
 const MovieReviews = ({reviews = {}}) => {
+  const firstReviewColumn = reviews.slice(0, Math.round(reviews.length / 2));
+  const secondReviewColumn = reviews.slice(Math.round(reviews.length / 2));
+
   return (
     <>
       <div className="movie-card__reviews movie-card__row">
         <div className="movie-card__reviews-col">
-          {reviews.map((review) => <MovieReview key={review.id} review={review}/>)}
+          {firstReviewColumn.map((review) => <MovieReview key={review.id} review={review}/>)}
+        </div>
+        <div className="movie-card__reviews-col">
+          {secondReviewColumn.map((review) => <MovieReview key={review.id} review={review}/>)}
         </div>
       </div>
     </>
