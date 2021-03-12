939eeb1e141373d88267c11bdba0915fe9ff3297
Fifer2 <KuryakovDenis@yandex.ru>
2021-03-12 18:35:09 +0700

    feat(store): joining store for application

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import App from "./components/app/app";
 import {movies, promoMovie} from "./mocks/movies";
 import reviews from "./mocks/reviews";
+import {createStore} from "redux";
+import {reducer} from "./store/reducer";
+import {Provider} from "react-redux";
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-    <App
-      movies = {movies}
-      reviews = {reviews}
-      promoMovie = {promoMovie}
-    />,
+    <Provider store={store}>
+      <App
+        movies = {movies}
+        reviews = {reviews}
+        promoMovie = {promoMovie}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
