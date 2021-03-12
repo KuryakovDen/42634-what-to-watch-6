e19a31becf71ab3d1fffe9780aa8cf97d53ea799
Fifer2 <KuryakovDenis@yandex.ru>
2021-03-12 18:47:44 +0700

    feat(store): joining devtools for application

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+
 import App from "./components/app/app";
 import {movies, promoMovie} from "./mocks/movies";
 import reviews from "./mocks/reviews";
-import {createStore} from "redux";
 import {reducer} from "./store/reducer";
-import {Provider} from "react-redux";
 
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    composeWithDevTools()
+);
 
 ReactDOM.render(
     <Provider store={store}>
