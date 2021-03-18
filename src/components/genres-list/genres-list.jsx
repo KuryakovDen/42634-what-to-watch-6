4cd7453127888220d6f4898ba04c3f798db41fdc
Fifer2 <KuryakovDenis@yandex.ru>
2021-03-18 20:59:35 +0700

    refactor(movie-list): refactoring for component

--- src/components/genres-list/genres-list.jsx
+++ src/components/genres-list/genres-list.jsx
@@ -3,11 +3,10 @@ import {connect} from 'react-redux';
 
 import {genresType, moviesType} from "../../validation";
 import {ActionCreator} from "../../store/action";
-import {DEFAULT_GENRE} from "../../const";
 import {movies} from "../../mocks/movies";
 import {getAllGenres} from "../../utils";
 
-const GenresList = ({activeGenre = DEFAULT_GENRE, setGenreAction = {}}) => {
+const GenresList = ({activeGenre, setGenreAction = {}}) => {
   const genres = getAllGenres(movies);
 
   const genreHandler = (evt, currentGenre) => {
