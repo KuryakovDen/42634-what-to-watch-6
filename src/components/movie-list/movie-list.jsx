445ff85687535322986043e173b9534377645372
Fifer2 <KuryakovDenis@yandex.ru>
2021-03-18 20:41:00 +0700

    refactor(movie-list): refactoring for component

--- src/components/movie-list/movie-list.jsx
+++ src/components/movie-list/movie-list.jsx
@@ -3,20 +3,21 @@ import {connect} from 'react-redux';
 
 import MovieCard from "../movie-card/movie-card";
 import {moviesType} from "../../validation";
-import {DEFAULT_GENRE} from "../../const";
 import {filterMoviesOnGenre} from "../../utils";
 
-const MoviesList = ({movies = {}, genre = DEFAULT_GENRE}) => {
+const MoviesList = ({movies = [], genre}) => {
+  const filteredMovies = filterMoviesOnGenre(movies, genre);
+
   return (
     <div className="catalog__movies-list">
-      {filterMoviesOnGenre(movies, genre).map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
+      {filteredMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
     </div>
   );
 };
 
 const mapStateToProps = (state) => ({
-  movies: state.moviesList,
-  genre: state.genre
+  genre: state.activeGenre,
+  movies: state.moviesList
 });
 
 MoviesList.propTypes = {
