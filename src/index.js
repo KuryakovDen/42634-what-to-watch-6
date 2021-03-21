f6f0fd9ad20295120f94922e1a3aca96f3184bfe
Fifer2 <KuryakovDenis@yandex.ru>
2021-03-21 17:59:29 +0700

    feat(app): join api to store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from "react-redux";
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
 import {composeWithDevTools} from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
 import App from "./components/app/app";
 import {reducer} from "./store/reducer";
+import createAPI from "./services/api";
+
+const api = createAPI();
 
 const store = createStore(
     reducer,
-    composeWithDevTools()
+    composeWithDevTools(
+        applyMiddleware(thunk.withExtraArgument(api))
+    )
 );
 
 ReactDOM.render(
