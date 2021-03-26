d41134ee6b01e278e15e07a2b3616b5c4897cdac
Fifer2 <KuryakovDenis@yandex.ru>
2021-03-26 15:54:46 +0700

    fix(review): fix bug with review id

--- src/components/review/review.jsx
+++ src/components/review/review.jsx
@@ -7,54 +7,51 @@ import CommentForm from "../comment-form/comment-form";
 
 const Review = ({movie}) => {
   return (
-    <>
-      <section className="movie-card movie-card--full">
-        <div className="movie-card__header">
-          <div className="movie-card__bg">
-            <img src={movie.background_image} alt={movie.name}/>
-          </div>
-
-          <h1 className="visually-hidden">WTW</h1>
-
-          <header className="page-header">
-            <div className="logo">
-              <Link to="/" className="logo__link">
-                <span className="logo__letter logo__letter--1">W</span>
-                <span className="logo__letter logo__letter--2">T</span>
-                <span className="logo__letter logo__letter--3">W</span>
-              </Link>
-            </div>
-
-            <nav className="breadcrumbs">
-              <ul className="breadcrumbs__list">
-                <li className="breadcrumbs__item">
-                  <Link to={`/films/${movie.id}`} className="breadcrumbs__link">{movie.name}</Link>
-                </li>
-                <li className="breadcrumbs__item">
-                  <a className="breadcrumbs__link">Add review</a>
-                </li>
-              </ul>
-            </nav>
-
-            <div className="user-block">
-              <div className="user-block__avatar">
-                <img src="img/avatar.jpg" alt="User avatar" width="63" height="63"/>
-              </div>
-            </div>
-          </header>
-
-          <div className="movie-card__poster movie-card__poster--small">
-            <img src={movie.poster_image} alt={movie.name} width="218"
-              height="327"/>
-          </div>
+    <section className="movie-card movie-card--full">
+      <div className="movie-card__header">
+        <div className="movie-card__bg">
+          <img src={movie.background_image} alt={movie.name}/>
         </div>
 
-        <div className="add-review">
-          <CommentForm></CommentForm>
-        </div>
+        <h1 className="visually-hidden">WTW</h1>
 
-      </section>
-    </>
+        <header className="page-header">
+          <div className="logo">
+            <Link to="/" className="logo__link">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </Link>
+          </div>
+
+          <nav className="breadcrumbs">
+            <ul className="breadcrumbs__list">
+              <li className="breadcrumbs__item">
+                <Link to={`/films/${movie.id}`} className="breadcrumbs__link">{movie.name}</Link>
+              </li>
+              <li className="breadcrumbs__item">
+                <a className="breadcrumbs__link">Add review</a>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="user-block">
+            <div className="user-block__avatar">
+              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63"/>
+            </div>
+          </div>
+        </header>
+
+        <div className="movie-card__poster movie-card__poster--small">
+          <img src={movie.poster_image} alt={movie.name} width="218"
+            height="327"/>
+        </div>
+      </div>
+
+      <div className="add-review">
+        <CommentForm></CommentForm>
+      </div>
+    </section>
   );
 };
 
