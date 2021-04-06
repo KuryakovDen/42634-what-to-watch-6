53938d77dc78795cbe0ae947d67fa4b5d102d9da
Fifer2 <KuryakovDenis@yandex.ru>
2021-04-06 13:50:07 +0700

    fix(comment-form): fix bug with empty comment (B1)

--- src/components/comment-form/comment-form.jsx
+++ src/components/comment-form/comment-form.jsx
@@ -59,6 +59,7 @@ const CommentForm = ({onSubmit, id}) => {
           name="review-text"
           id="review-text"
           placeholder="Review text"
+          required={true}
         />
         <div className="add-review__submit">
           <button className="add-review__btn" type="submit">Post</button>
